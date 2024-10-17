3cb555b75c9f66c6b0c8b304841f0e59f4b3dd04
reyunj <junrey1296@gmail.com>
2024-10-17 16:22:48 +0800

    Remove embedded content

---
+++ app/bracket/[bracketId]/page.js
@@ -0,0 +1,16 @@
+"use client";
+import { useParams } from "next/navigation";
+
+export default function BracketPage() {
+  const { bracketId } = useParams(); // Get the bracket ID from the URL
+
+  const iframeSrc = `https://brackethq.com/b/${bracketId}/embed/?zoom=0`;
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="w-full max-w-4xl">
+        <iframe src={iframeSrc} width="100%" height="550"></iframe>
+      </div>
+    </div>
+  );
+}
