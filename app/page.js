cf36d8e6c19dc35448359a681bf1743b1a9fd87f
reyunj <junrey1296@gmail.com>
2024-10-17 20:10:14 +0800

    Responsive home

--- app/page.js
+++ app/page.js
@@ -9,25 +9,24 @@ export default function Home() {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault(); // Prevent form submission
-    // Open the new page in a new tab
+    e.preventDefault();
     window.open(`/bracket/${bracketId}`, "_blank");
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="w-full max-w-4xl mb-4">
-        <form onSubmit={handleSubmit} className="flex">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md mb-4 px-4">
+        <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row">
           <input
             type="text"
             value={bracketId}
             onChange={handleBracketIdChange}
             placeholder="Enter bracket ID"
-            className="border p-2 w-full"
+            className="border border-gray-300 p-3 rounded-md w-full sm:w-3/4 mb-2 sm:mb-0 sm:mr-2 transition duration-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
           />
           <button
             type="submit"
-            className="ml-2 border p-2 bg-blue-500 text-white"
+            className="border border-transparent p-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300"
           >
             Submit
           </button>
